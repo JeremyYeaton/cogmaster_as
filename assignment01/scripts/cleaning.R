466235f7df9ae9675fc4904176539c778fee46cf
Jeremy Yeaton <jdyeaton27@gmail.com>
2017-11-25 16:45:15 +0100

    commit with clean data for assignment01, pre question responses

---
+++ assignment01/scripts/cleaning.R
@@ -0,0 +1,20 @@
+###IMPORT LIBRARIES####
+library(dplyr)
+library(magrittr)
+
+###READ IN DATA####
+f <-read.table("data/smoking-data.dat",header=FALSE)
+names(f)
+
+#RENAME COLUMNS
+colnames(f)<-cbind("cat","fev1")
+
+#RENAME VARIABLES
+f_clean <- f %>%
+  mutate(cat.f= factor(if_else(cat=="1","non-smoker",
+                        ifelse(cat=="2","early",
+                               ifelse(cat=="3","recent",
+                                      ifelse(cat=="4","current","")))),
+                       ordered=T))
+
+f_clean
